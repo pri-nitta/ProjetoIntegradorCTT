142cc807b62d20457e2b902159f4c6a5494f69ef
Pri Nitta <egashira.sayuri@gmail.com>
2021-03-10 00:06:04 -0300

    Initial commit

---
+++ app/src/main/java/com/ctt/projetointegradorctt/Tarefas_Visualizar.kt
@@ -0,0 +1,11 @@
+package com.ctt.projetointegradorctt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tarefas_Visualizar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tarefas__visualizar)
+    }
+}
